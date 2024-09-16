bde04ea93bd37fda27c30a6de03c00a3834e7fe5
github keita7777 <github mine0kei@gmail.com>
2024-09-17 03:00:20 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+import LoadingCards from "@/components/card/LoadingCards";
+import CategoriesList from "@/components/home/CategoriesList";
+import PropertiesContainer from "@/components/home/PropertiesContainer";
+
+import { Suspense } from "react";
+
+function Homepage({
+  searchParams,
+}: {
+  searchParams: { category?: string; search?: string };
+}) {
+  return (
+    <section>
+      <CategoriesList
+        category={searchParams.category}
+        search={searchParams.search}
+      />
+      <Suspense fallback={<LoadingCards />}>
+        <PropertiesContainer
+          category={searchParams.category}
+          search={searchParams.search}
+        />
+      </Suspense>
+    </section>
+  );
+}
+
+export default Homepage;
